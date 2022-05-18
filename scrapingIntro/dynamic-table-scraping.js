5d9ed722fac77488ca3779f4174f3651ec60dcee
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-05-18 04:48:39 +0200

    Scraping HTML tables with Request/Cheerio

--- scrapingIntro/dynamic-table-scraping.js
+++ scrapingIntro/dynamic-table-scraping.js
@@ -4,20 +4,24 @@ const cheerio = require('cheerio');
 async function main() {
     const result = await request.get('https://www.codingwithstefan.com/table-example/');
     const $ = await cheerio.load(result);
-    // $("body > table > tbody > tr > td").each((i, e) => {
-    //     console.log($(e).text())  
-    // })
 
     const scrapedRows = [];
+    const tableHeaders = [];
 
     $("body > table > tbody > tr").each((i, e) => {
-        if (i === 0) return true;
+        if (i === 0) {
+            const ths = $(e).find("th");
+            ths.each((i, e) => {
+                tableHeaders.push($(e).text().toLowerCase());
+            })
+            return true;
+        }
         const tds = $(e).find("td");
-        const company = $(tds[0]).text();
-        const contact = $(tds[1]).text();
-        const country = $(tds[2]).text();
-        const scrapedRow = { company, contact, country };
-        scrapedRows.push(scrapedRow);
+        const tableRow = {};
+        tds.each((i, e) => {
+            tableRow[tableHeaders[i]] = $(e).text();    
+        })
+        scrapedRows.push(tableRow);
     })
     console.log(scrapedRows);
 }
